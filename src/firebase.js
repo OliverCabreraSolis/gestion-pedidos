ec773151bc09d5b120339a2e8029f79c450bbb34
Oliver Cabrera <166546755+OliverCabreraSolis@users.noreply.github.com>
2026-01-12 13:46:28 -0500

    usar variables de entorno firebase

--- src/firebase.js
+++ src/firebase.js
@@ -1,19 +1,14 @@
-// Importamos lo necesario de Firebase
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
-// Configuración de tu proyecto Firebase
 const firebaseConfig = {
-  apiKey: "AIzaSyBuxFLUXE_hQFA8SMYoq1lOxwraXCRxZvM",
-  authDomain: "pedidosexa-8688d.firebaseapp.com",
-  projectId: "pedidosexa-8688d",
-  storageBucket: "pedidosexa-8688d.firebasestorage.app",
-  messagingSenderId: "364295113744",
-  appId: "1:364295113744:web:4e61f5af4e9c44e8e3ce95",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Inicializamos Firebase
 const app = initializeApp(firebaseConfig);
-
-// Exportamos la base de datos
 export const db = getFirestore(app);
